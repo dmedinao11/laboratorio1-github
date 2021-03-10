5f9f2fc8ee6c01321cf57fa3d304c6d85ee6a310
Daniel Medina <dmedinao@unal.edu.co>
2021-03-10 12:24:59 -0500

    Añadir funcionalidad

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringReverse sr = new StringReverse();
-        String a = sr.reverseString("Hello");
-        System.out.println(a);
-    }
-}
--- src/StringReverser.java
+++
@@ -1,9 +0,0 @@
-public class StringReverser {
-    public String reverseString(String s){
-        String reversedString = "";
-        for(int i = s.length() - 1; i >= 0; i--){
-            reversedString = reversedString + s.charAt(i);
-        }
-        return reversedString;
-    }
-}
